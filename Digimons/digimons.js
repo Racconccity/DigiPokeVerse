8fe11e832484ef7cb8e410a0ab71512c723d2a17
Racconccity <71821238+Racconccity@users.noreply.github.com>
2020-11-24 13:26:36 +0100

    Update digimons.js

--- Digimons/digimons.js
+++ Digimons/digimons.js
@@ -165,6 +165,13 @@ module.exports.run = async(client, message, args) => {
     message.channel.send("El digimon es: " + img.replace(/\.[^.$]+$/, ''))
     message.channel.send("Nivel: "+ Math.floor(Math.random()*(99+1)))
     message.channel.send({ file: ["Digimons/"] + img })
+
+    if (message.content.startsWith(prefix +"digimons")){
+      message.channel.send({embed: {
+        color: 3447003,
+        description: ""
+      }});
+    }
 }
 
 module.exports.config = {
