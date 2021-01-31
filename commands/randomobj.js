7f16f634f71bdd490a119a0875241a781bdf1524
Racconccity <kingdomofangmar@gmail.com>
2021-01-31 17:28:43 +0100

    Update randomobj.js

--- commands/randomobj.js
+++ commands/randomobj.js
@@ -3,10 +3,10 @@ const config = require("../config.json");
 const colours = require("../colours.json");
 
 module.exports.run = async(client, message, args) => {
-  var randomimg = path.join(__dirname, 'Pokemon/');
-  randomimg = [
-    "Espada","Escudo","Hacha","Pico","Palo","Lanza"
-  ]
+    var randomimg = path.join(__dirname, 'commands/');
+    randomimg = [
+        "Espada", "Escudo", "Hacha", "Pico", "Palo", "Lanza"
+    ]
     const img = randomimg[Math.floor(Math.random() * randomimg.length - 1 + 1)]
     message.channel.send("--------------------------------------------------")
     message.channel.send("El objeto es: " + img.replace(/\.[^.$]+$/, ''))
