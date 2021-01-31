af3f2de81e458f8941e97ad9989b0533f7ea703b
Racconccity <71821238+Racconccity@users.noreply.github.com>
2021-01-31 16:10:50 +0100

    Update randomobj.js

--- commands/randomobj.js
+++ commands/randomobj.js
@@ -3,10 +3,15 @@ const config = require("../config.json");
 const colours = require("../colours.json");
 
 module.exports.run = async(client, message, args) => {
-  function arrayContains(needle, arrhaystack)
-    {
-      return (arrhaystack.indexOf(needle) > -1);
+  var listado:Array = Array("Guillermo", "Lucas", "David", "Roger", "Bruno", "Iñaki", "Alex", "Carlos");
+    var Total:Number = listado.length;
+    for (i=0; i&lt;Total; i++) {
+      aleatorio = Math.floor(Math.random()*(listado.length));
+      seleccion = listado[aleatorio];
+      trace(seleccion);
+      listado.splice(aleatorio, 1);
     }
+    message.channel.send(aleatorio);
 }
 
 module.exports.config = {
