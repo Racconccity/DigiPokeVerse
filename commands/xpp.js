7cdceecb1ab2b8d92c6ebebce13eecae669e626a
Racconccity <kingdomofangmar@gmail.com>
2021-01-31 17:44:59 +0100

    a7

--- commands/xpp.js
+++ commands/xpp.js
@@ -3,11 +3,11 @@ const config = require("../config.json");
 const colours = require("../colours.json");
 
 module.exports.run = async(client, message, args) => {
-    message.channel.send("Obtienes "  + Math.floor(Math.random()*(99+1)) +  " monedas de cobre")
+    message.channel.send("Obtienes "  + Math.floor(Math.random()*(199+1)) +  " puntos de experiencia")
 }
 
 module.exports.config = {
-    name: "coin",
-    usage: "-coin",
-    aliases: ["coin"]
+    name: "xpp",
+    usage: "-xpp",
+    aliases: ["xpp"]
 }
