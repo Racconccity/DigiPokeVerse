0456ff8d6218a75b7667893966ee7a9b4a381626
Racconccity <71821238+Racconccity@users.noreply.github.com>
2021-01-31 16:06:00 +0100

    Update randomobj.js

--- commands/randomobj.js
+++ commands/randomobj.js
@@ -3,24 +3,10 @@ const config = require("../config.json");
 const colours = require("../colours.json");
 
 module.exports.run = async(client, message, args) => {
-      var myArray = [
-        "Hacha", "Escudo", "Espada"
-      ];
-
-    function getRandomString(array){
-      return array[Math.floor(Math.random()*array.length)]
+  function arrayContains(needle, arrhaystack)
+    {
+      return (arrhaystack.indexOf(needle) > -1);
     }
-
-    function getRandomStrings(numero, array){
-      var strings = [];
-      while(strings.length < numero){
-        var string = getRandomString(array);
-        if(strings.indexOf(string) == -1) strings.push(string);
-      }
-        return strings;
-    }
-
-    console.log(getRandomStrings(4, myArray));
 }
 
 module.exports.config = {
