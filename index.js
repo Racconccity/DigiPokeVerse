d6e8c8e3bff2e9baa932b2a2746e579f6cb6abc3
Racconccity <kingdomofangmar@gmail.com>
2021-02-28 00:06:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -135,26 +135,6 @@ client.on('message', message => {
 });
 //////////////////////////////////////////////////////////////////////////////////////
 client.on('message', message => {
-  var prefix = '-'
-    if (message.content === '-ca') {
-      setInterval(function time(){
-        var d = new Date();
-        var hours = 24 - d.getHours();
-        var min = 60 - d.getMinutes();
-        if((min + '').length == 1){
-          min = '0' + min;
-        }
-        var sec = 60 - d.getSeconds();
-        if((sec + '').length == 1){
-              sec = '0' + sec;
-        }
-        jQuery('#the-final-countdown p').html(hours+':'+min+':'+sec)
-      }, 1000);
-      message.channel.send(time() + ' tiempo restante');
-    }
-});
-//////////////////////////////////////////////////////////////////////////////////////
-client.on('message', message => {
 
     var sender = message.author;
     var msg = message.content.toUpperCase();
